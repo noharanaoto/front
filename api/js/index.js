a47201082fd747a3baec28545ea4b4cdbc97f972
noharanaoto <ibusan101@gmail.com>
2022-11-19 13:42:09 +0900

    add : apiで記事の情報を表示

--- api/js/index.js
+++ api/js/index.js
@@ -1,13 +1,56 @@
-// 関数を2回実行する関数！！
-function doTwice(func) {
-  func(); // 1回目！
-  func(); // 2回目！
+// function doTwice(func) {
+//   func(); 
+//   func(); 
+// }
+
+// function hello() {
+//   console.log('Hello!');
+// }
+
+// doTwice(hello);
+
+const hackerUriPrefix = 'https://hacker-news.firebaseio.com/v0/'
+
+// 最新記事のidリストを取得する
+async function getLatestPostIds(){
+  try{
+    const res = await fetch(`${hackerUriPrefix}newstories.json`)
+    return res.json()
+  } catch(e) {
+    console.error(e)
+    return []
+  }
 }
 
-// あいさつするだけの関数
-function hello() {
-  console.log('Hello!');
+async function getPostData(id){
+  try{
+    const res = await fetch(`${hackerUriPrefix}item/${id}.json`)
+    return res.json()
+  } catch(e) {
+    console.error(e)
+    return null
+  }
 }
 
-// あいさつを2回実行する
-doTwice(hello);
+function createPostDOM (post){
+  const postLinkNode = document.createElement('a')
+
+  postLinkNode.classList.add('posts_item')
+  postLinkNode.setAttribute('href', post.url || '#')
+  postLinkNode.setAttribute('target', post.url ? '_blank' : '_self')
+  postLinkNode.textContent = post.title
+
+  document.getElementById('posts').appendChild(postLinkNode)
+}
+
+async function getPosts(limit){
+  try{
+    const ids = await getLatestPostIds()
+    const postDataList = await Promise.all(ids.slice(0,limit).map(id => getPostData(id)))
+    postDataList.forEach(post => createPostDOM(post))
+  }catch(e){
+    console.error(e)
+  }
+}
+
+getPosts(4)
